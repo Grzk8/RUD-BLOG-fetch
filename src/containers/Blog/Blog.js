11b032ede78fff9bba583220e81d672d133a92da
Grzegorz Kaczor <grzesiek125@op.pl>
2020-11-03 10:43:40 +0100

    routing newpost

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,63 +1,39 @@
 import React, { Component } from 'react';
+import { Route, NavLink } from 'react-router-dom';
 
-import Post from '../../components/Post/Post';
-import FullPost from '../../components/FullPost/FullPost';
-import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
+import Posts from './Posts/Posts';
+import NewPost from './NewPost/NewPost';
+
 
 class Blog extends Component {
-    state = {
-        posts: [],
-        selectedPostId: null,
-        error: false
-    }
-
-    componentDidMount () {
-        fetch('https://jsonplaceholder.typicode.com/posts')
-        .then(response => response.json())
-        .then (resp => {
-            const posts = resp.slice(0, 4);
-            const updatedPosts = posts.map(post =>{
-                return{
-                    ...post,
-                    author: 'Max'
-                }
-            })
-            this.setState({posts: updatedPosts})
-        })
-        .catch(error => {
-            this.setState({error: true})
-        })
-    }
-
-    postSelectedHandler = (id) => {
-        this.setState({selectedPostId: id});
-    }
 
     render () {
-        let posts = <p style={{textAlign: 'center', color: 'red'}}>Something went wrong</p>;
-        if (!this.state.error) {
-            posts = this.state.posts.map(post => {
-                return <Post 
-                key={post.id} 
-                title={post.title} 
-                author={post.author}
-                clicked={() => this.postSelectedHandler(post.id)}/>
-            }
-        )
-        }
         return (
-            <div>
-                <section className="Posts">
-                    {posts}
-                </section>
-                <section>
-                    <FullPost id={this.state.selectedPostId}/>
-                </section>
-                <section>
-                    <NewPost />
-                </section>
-            </div>
+                <div className="Blog">
+                    <header>
+                        <nav>
+                            <ul>
+                                <li><NavLink to='/' 
+                                exact
+                                activeClassName='my-active'
+                                activeStyle={{
+                                    color: '#fa921f',
+                                    textDecoration: 'underline'
+                                }}>Home</NavLink></li>
+                                <li><NavLink to={{
+                                    pathname: '/new-post',
+                                    hash: '#submit',
+                                    search: '?quick-submit=true'
+                                }}>New Post</NavLink></li>
+                            </ul>
+                        </nav>
+                    </header>
+                    {/* <Route path="/" exact render={()=> <h1>Home</h1>} />
+                    <Route path="/" render={()=> <h2>Home 2</h2>} /> */}
+                    <Route path="/" exact component={Posts}/>
+                    <Route path="/new-post" exact component={NewPost}/>
+                </div>
         );
     }
 }
